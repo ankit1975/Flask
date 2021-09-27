373f5974749fc9dcc73796e77b1982191af96811
Ankit Malpani <ankitmalpani1975@gmail.com>
2021-09-27 16:32:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/index')
 def index():
     name = request.args.get('name')
     return render_template('index.html', name = name)
@@ -13,9 +13,13 @@ def skill():
     vid = yt.spliturl('Python Developer')
     return render_template('skill.html', skill = vid)
 
-@app.route('/video')
+@app.route('/')
 def video():
     vid = request.args.get('vid')
+    # print(vid)
+
+    if vid == None:
+        vid = '2mzH2dbZWYo'
     return render_template('video.html', vid = vid)
 
 @app.errorhandler(404)
