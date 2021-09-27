5028f9a874a7c757c62c7b83ed1ff732aca9289f
Ankit Malpani <ankitmalpani1975@gmail.com>
2021-09-27 16:13:05 +0530

    commited

---
+++ app.py
@@ -0,0 +1,26 @@
+
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    name = request.args.get('name')
+    return render_template('index.html', name = name)
+    
+@app.route('/skill')
+def skill():
+    from anki import seperate as yt 
+    vid = yt.spliturl('Python Developer')
+    return render_template('skill.html', skill = vid)
+
+@app.route('/video')
+def video():
+    link = request.args.get('vid')
+    return render_template('video.html', vid = vid)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return ( render_template('404.html'), 404 )
+
+if __name__ == '__main__':
+    app.run(debug=True)
